244bd44dece0793e5afcbf5b854b8dd163cee82e
Markus Thill <mthill@tesla.gm.FH-Koeln.DE>
2019-10-25 11:30:21 +0200

    Added a few .cpu() to be able to run the stuff on GPUs

--- src/algorithms/lstm_enc_dec_axl.py
+++ src/algorithms/lstm_enc_dec_axl.py
@@ -90,8 +90,8 @@ class LSTMED(Algorithm, PyTorchUtils):
             score = -mvnormal.logpdf(error.view(-1, X.shape[1]).data.cpu().numpy())
             scores.append(score.reshape(ts.size(0), self.sequence_length))
             if self.details:
-                outputs.append(output.data.numpy())
-                errors.append(error.data.numpy())
+                outputs.append(output.data.cpu().numpy())
+                errors.append(error.data.cpu().numpy())
 
         # stores seq_len-many scores per timestamp and averages them
         scores = np.concatenate(scores)
